6021b931702c671a2830d545edcbcece987a7bb6
shengkai <maimemdada@gmail.com>
2023-02-02 10:48:18 +0700

    user chose oxford or paris

--- server.py
+++ server.py
@@ -1,27 +1,70 @@
 import os
 import numpy as np
 from PIL import Image
+from pathlib import Path
 from feature_extractor import FeatureExtractor
 from datetime import datetime
 from flask import Flask, request, render_template
-from pathlib import Path
+
 
 app = Flask(__name__)
 
-data_folder = r"static\image"
-feature_folder = r"static\feature"
+
+# User choose between looking for in Oxford data or Paris data, default is Oxford
+selected_data_folder = r"static\dataset\Oxford5k"
 
 #read image features
 fe = FeatureExtractor()
 features = []
 image_paths = []
 
-for feature_path in os.listdir(feature_folder):
-    feature_path_full = os.path.join(feature_folder, feature_path)
-    features.append(np.load(feature_path_full))
-    image_paths.append(os.path.join(data_folder, os.path.splitext(feature_path)[0] + ".jpg"))
+if selected_data_folder == r"static\dataset\Paris6k": # if Paris
+    selected_feature_folder = r"static\feature\paris_feature"
 
+    for feature_path in os.listdir(selected_feature_folder):
+        '''
+        feature_path : "paris_defense_000000.npy"
+        +
+        selected_feature_folder : "static\feature\paris_feature"
+        =
+        feature_path_full : "static\feature\paris_feature\paris_defense_000000.npy"
+        ______________________________________________________________
+        selected_data_folder : "static\dataset\Paris6k"
+        +
+        feature_path - ".npy" + ".jpg": "paris_defense_000000.jpg"
+        =
+        image_paths : "static\dataset\Paris6k\paris_defense_000000.jpg"
+        '''
+
+        feature_path_full = os.path.join(selected_feature_folder, feature_path)
+        features.append(np.load(feature_path_full))
+        image_paths.append(os.path.join(selected_data_folder, os.path.splitext(feature_path)[0] + ".jpg"))
+
+else: # if Oxford
+    selected_feature_folder = r"static\feature\oxford_feature"
+
+    for feature_path in os.listdir(selected_feature_folder):
+        '''
+        feature_path : "all_souls_000000.npy"
+        +
+        selected_feature_folder : "static\feature\oxford_feature"
+        =
+        feature_path_full : "static\feature\oxford_feature\all_souls_000000.npy"
+        ______________________________________________________________
+        selected_data_folder : "static\dataset\Oxford5k"
+        +
+        feature_path - ".npy" + ".jpg": "all_souls_000000.jpg"
+        =
+        image_paths : "static\dataset\Oxford5k\all_souls_000000.jpg"
+        '''
+
+        feature_path_full = os.path.join(selected_feature_folder, feature_path)
+        features.append(np.load(feature_path_full))
+        image_paths.append(os.path.join(selected_data_folder, os.path.splitext(feature_path)[0] + ".jpg"))
+
+# convert features to numpy array
 features = np.array(features)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -44,4 +87,4 @@ def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
