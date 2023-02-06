2b7d7fabd4bc33de24048b7ab380b8f0f7fbb1bc
shengkai <maimemdada@gmail.com>
2023-02-06 21:36:22 +0700

    check type .jpg file

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,26 @@
-const fileInput = document.getElementById("file-input");
+const fileInput = document.getElementById('file-input');
 const reviewImage = document.querySelector('.review-image');
-// const reviewImage = document.getElementById('taolao');
+const errorMessage = document.querySelector('.format-note');
 
 fileInput.addEventListener('change', function () {
     const file = this.files[0];
     const reader = new FileReader();
+
+    //check if the file is jpg or not
+    // if (file.type !== 'image/jpeg') {
+    //     alert("Error: Please upload a .jpg file!");
+    //     return;
+    // }
+    
+    //check if the file is jpg or not
+    if (file.type !== "image/jpeg") {
+        errorMessage.innerHTML = "Error: file type must be .jpg!";
+        errorMessage.style.color = '#E51515';
+        return;
+    } else {
+        errorMessage.innerHTML = "Only .jpg files are accepted";
+        errorMessage.style.color = '#5A5A5A';
+    }
 
     reader.addEventListener('load', function () {
         reviewImage.src = reader.result;
